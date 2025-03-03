8708bbccbb40ecb8457f326d9e4205b238c67ef1
0x4248 <60709927+0x4248@users.noreply.github.com>
2025-03-03 10:36:11 +0000

    Update init
    Signed-off-by: 0x4248 <60709927+0x4248@users.noreply.github.com>

--- build.sh
+++ build.sh
@@ -23,5 +23,12 @@ cd /tmp/busybox
 mkdir -p dev
 mknod dev/console c 5 1
 mknod dev/ram b 1 0
+mknod dev/tty c 5 0
+mknot dev/tty1 c 4 1
+mknod dev/fb0 c 29 0
 find -print0 | cpio -0oH newc | gzip -9 > /mnt/initramfs.cpio.gz
+# make a .qcow2 image of the _install so we can test the second stage
+qemu-img create -f qcow2 /tmp/busybox.qcow2 1G
+# put _/install in the qcow2 image
+
 
